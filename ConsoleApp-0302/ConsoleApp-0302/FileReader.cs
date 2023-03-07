bd60a7da5050429f6c78acb3a320e28aebef4aab
HironobuKawahito0213 <111252810+HironobuKawahito0213@users.noreply.github.com>
2023-03-07 18:07:35 +0900

    Add files via upload

---
+++ ConsoleApp-0302/ConsoleApp-0302/FileReader.cs
@@ -0,0 +1,72 @@
+﻿using Magnum.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleApp_0302
+{
+    internal class FileReader
+    {
+        //手入力
+        private readonly string fileName = "C:\\Users\\ia\\Desktop\\Book1.csv";
+        
+        //列数
+        public int length = 11;
+
+        //行数
+        public int column = 10;
+
+        public List<List<string>> lines = new List<List<string>>();
+
+        public List<List<string>> fileRead()
+        {
+            List<string> ls = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    while (0 <= sr.Peek())
+                    {
+                        
+                        for (int i = 0; i < length ; i++)
+                        {
+                            //カンマ区切りで分割して配列で格納する
+                            var line = sr.ReadLine()?.Split(',');
+                            if (line is null) continue;
+
+
+                            for (int j = 0; j < length; j++)
+                            {
+                                ls.Add(line[j]);
+                            }
+
+                            //リストにデータを追加する
+                            lines.Add(ls);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (lines[i][j].Equals(" "))
+                    {
+                        lines[i][j] = null; ;
+                    }
+                }
+            }
+            return lines;
+            
+        }
+    }
+}
---
+++ ConsoleApp-0302/ConsoleApp-0302/FundDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_0302
+{
+    internal class FundDTO
+    {
+        public string SSS_ID;
+        public string Fund_Short_Name;
+        public string Legal_Entity_Id;
+        public string Share_Partner_Class_Id;
+        public string SSS_Description;
+        public string Inception_Date;
+        public string Termination_Date;
+        public string Switch_Date;
+        public string New_SSS_ID;
+        public string New_Fund_Short_Name;
+        public string Update_Time;
+    }
+}
